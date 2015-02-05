1d489484647999f77d3cf97342bed542f7676c0b
RemiVR <remi.vr5@gmail.com>
2015-02-05 23:24:51 +0100

    Email and phone number added with no errors(typos...)

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,6 @@
 class Contact < ActiveRecord::Base
 	has_many :phone_numbers
-	has_many :email_adresses
+	has_many :email_addresses
 
 	validates :name, presence: true, uniqueness: true
 	validates :address, presence: true
